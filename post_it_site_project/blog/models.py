71f1c196b5fc24c6f64ab9ca44d53c7ba6008f60
Bigbys-Hand <lenomdeguerre75@gmail.com>
2020-05-03 20:33:53 -0700

    Learned about using Django's ORM feature to interact with the SQL DB via Python.

--- post_it_site_project/blog/models.py
+++ post_it_site_project/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+# Django makes each class its own table in the DB
+# This class governs content posts. It has title, content, date_posted, and author functions, which will be DB fields.
+# We did not add parentheses on timezone.now so that we can call the function repeatedly instead of running it now.
+# We import the User class so that each post can have an author. 'on_delete' is meant to handle the post if the author
+# is deleted. 'models.CASCADE' means that when the user-author is deleted, so is the post.
--- post_it_site_project/blog/views.py
+++ post_it_site_project/blog/views.py
@@ -1,27 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
+
+
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Basch_the_Strong',
-        'title': 'More remedies?',
-        'content': 'Where can I find a caster that knows Remedy? Need to kill a wraith.',
-        'date_posted': 'May 2 2020'
-    },
-    {
-        'author': 'VaanBro_92',
-        'title': 'RE: More remedies?',
-        'content': 'Basch, that bounty isn\'t going anywhere, we\'ll get back to Rabanaster soon enough.',
-        'date_posted': 'May 2 2020'
-    }
-]
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
